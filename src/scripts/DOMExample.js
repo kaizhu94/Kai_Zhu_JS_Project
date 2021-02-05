0e7b3df9db65ff5d7eb4d9ac9fe7960924916430
kaizhu94 <71399999+kaizhu94@users.noreply.github.com>
2021-02-05 16:41:35 -0500

    Initial commit

---
+++ src/scripts/DOMExample.js
@@ -0,0 +1,60 @@
+import stitchAndYoda from "../images/yoda-stitch.jpg";
+
+export const DOMExample = () => {
+  const testObj = {
+    key1: "hi",
+    key2: {
+      key3: "Hello",
+    },
+  };
+
+  const greeting = testObj?.key2?.key3 || testObj.key1;
+  document.body.classList.add("center");
+  const card = document.createElement("div");
+  card.classList.add("card", "center");
+  card.innerHTML = `<h2>${greeting} World!</h2>`;
+  document.body.append(card);
+  const imgCard = document.createElement("div");
+  imgCard.classList.add("card", "center", "image-card");
+  document.body.appendChild(imgCard);
+  const imgElement = document.createElement("img");
+  imgElement.src = buildAssetPath(stitchAndYoda);
+  imgElement.classList.add("card", "center");
+  imgElement.style.objectFit = "cover";
+  imgElement.style.filter = "grayscale(80%)";
+  document.body.appendChild(imgElement);
+  const randomCard = document.createElement("div");
+  randomCard.classList.add("card", "center", "rando");
+  const winnerP = document.createElement("p");
+  const randomButton = document.createElement("btn");
+  randomButton.innerText = "Pick Me!";
+  randomCard.append(randomButton, winnerP);
+  document.body.append(randomCard);
+  randomButton.onclick = (e) => {
+    e.preventDefault();
+    winnerP.innerHTML = randomJobSeeker();
+  };
+};
+
+// to resolve path issues for images in JS
+
+function buildAssetPath(imgSrc) {
+  return `./dist/${imgSrc}`;
+}
+
+function randomJobSeeker() {
+const jobSeekers = process.env.NODE_ENV === 'development' ? 
+  require('../../secret').JOBSEEKERS : 
+  [
+    "Arthur Dent",
+    "Ford Prefect",
+    "Zaphod Beeblebrox",
+    "Marvin the Paranoid Android",
+    "Trillian",
+    "Slartibartfast",
+  ];
+
+  const winner = jobSeekers[Math.floor(Math.random() * jobSeekers.length)];
+  return winner;
+}
+
