f66c5d1684f698e332b335a520f0f2eb6c996fff
KaiZhu <zhukai0803@gmail.com>
2021-02-13 11:44:27 -0500

    add e.prevent

--- src/index.js
+++ src/index.js
@@ -49,13 +49,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let selector = document.getElementById('images-selector')
     let meme = '';
     selector.addEventListener('click', e=>{
-        // e.preventDefault();
+        e.preventDefault();
         meme = e.currentTarget.value
     })
 
     const start = document.getElementById('start');
     start.addEventListener('click', e=>{
-        // e.preventDefault();
+        e.preventDefault();
         if(meme !== ''){
             if(game.playing){
                 alert("You have a game runing, use reset for a new puzzle.")
